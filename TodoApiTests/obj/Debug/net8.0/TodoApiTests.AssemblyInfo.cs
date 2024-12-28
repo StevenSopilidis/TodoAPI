066720392cb6e10669e872c01c7562241128849f
StevenSopi <stefanossopilidis2003@gmail.com>
2024-12-28 08:50:41 +0200

    added gitignore

--- TodoApiTests/obj/Debug/net8.0/TodoApiTests.AssemblyInfo.cs
+++ TodoApiTests/obj/Debug/net8.0/TodoApiTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApiTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25c06b046eeea12220a54fabff54bb00ccaac00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e390ffb84d54a0666132b7446f12a19d7810bb35")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApiTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApiTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
